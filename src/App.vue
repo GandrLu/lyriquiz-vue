671015c063a8cd6f216c4f770728224c4a13ffdf
GandrLu <acidyumyum@web.de>
2025-12-20 16:58:10 +0100

    Initial commit. Add spotify PKCE Authentication Flow. Add song fetch.

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import LoginSection from './components/LoginSection.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/lyriquiz_logo.png" width="125" height="125" />
+
+    <div class="wrapper">
+      <LoginSection msg="Lyriquiz" />
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
